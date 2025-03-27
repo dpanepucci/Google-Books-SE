7d8d2920632ec1d68765423601759037e5db6f43
Dylan Panepucci <dylan.panepucci@gmail.com>
2025-03-27 17:23:14 -0400

    added utils folder with auth.ts file for JWT auth. Working on resolvers.ts page

--- Develop/server/src/schemas/resolvers.ts
+++ Develop/server/src/schemas/resolvers.ts
@@ -1,7 +1,8 @@
 
 // CHANGE FILE TO MATCH WHAT THE PROJECT NEEDS. USE CONTROLLER.TS FILE FOR REFERENCE 
 
-import { Profile } from '../models/index.js'; // Will need to change import
+import { createUser, deleteBook, getSingleUser, login, saveBook } from '../controllers/user-controller.js';
+import { User } from '../models/User.js';
 import { signToken, AuthenticationError } from '../utils/auth.js';
 
 interface getSingleUser {
@@ -26,24 +27,27 @@ interface deleteBook {
 
 const resolvers = {
   Query: {
+    getSingleUser
 
   },
 
   Mutation: {
+    createUser,
+    saveBook,
+    deleteBook,
 
-    },
 
     login: async (_parent: unknown, { email, password }: { email: string; password: string }): Promise<{ token: string; profile: Profile }> => {
       // Find a profile by email
-      const profile = await Profile.findOne({ email });
+      const user = await User.findOne({ email });
 
-      if (!profile) {
+      if (!user) {
         // If profile with provided email doesn't exist, throw an authentication error
         throw AuthenticationError;
       }
 
       // Check if the provided password is correct
-      const correctPw = await profile.isCorrectPassword(password);
+      const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
         // If password is incorrect, throw an authentication error
@@ -51,9 +55,10 @@ const resolvers = {
       }
 
       // Sign a JWT token for the authenticated profile
-      const token = signToken(profile.name, profile.email, profile._id);
-      return { token, profile };
+      const token = signToken(user.name, user.email, user._id);
+      return { token, profile: user };
     },
+  },
 };
 
 export default resolvers;
