c619ed5a636b67029edd8e92376ad02b4fbbebca
Dylan Panepucci <dylan.panepucci@gmail.com>
2025-03-30 13:02:13 -0400

    mutations and queries updated but not completed. App.ts updated for Apollo Server, few more packages installed

--- Develop/client/src/utils/mutations.ts
+++ Develop/client/src/utils/mutations.ts
@@ -5,3 +5,22 @@
 // SAVE_BOOK execute 'saveBook'
 
 // REMOVE_BOOK execute 'removeBook'
+
+
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+ mutation 
+`;
+
+export const ADD_USER = gql`
+  mutation 
+`;
+
+export const SAVE_BOOK = gql`
+  mutation 
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation 
+`;
--- Develop/client/src/utils/queries.ts
+++ Develop/client/src/utils/queries.ts
@@ -1,1 +1,7 @@
 // Will hold the GET_ME, which will execute the 'me' query set up using the Apollo Server.
+
+import { gql } from '@apollo/client'
+
+export const QUERY_ME = gql`
+  query
+`;
