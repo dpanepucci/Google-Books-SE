f94d26465ade7487d3e4d27fd384bf707621231e
Dylan Panepucci <dylan.panepucci@gmail.com>
2025-04-06 15:07:02 -0400

    Server not connecting on render, working on bug to deploy

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -31,7 +31,7 @@ const startApolloServer = async () => {
   );
 
   if (process.env.NODE_ENV === 'production') {
-    const clientPath = path.resolve(__dirname, '../client/dist');
+    const clientPath = path.resolve('../client/dist');
     app.use(express.static(clientPath));
 
     app.get('*', (_req: Request, res: Response) => {
